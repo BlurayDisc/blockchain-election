bdfa162d24bed3feecfb365cd0b2b2bc9de48562
Blu <run_y_an@hotmail.com>
2021-02-23 02:16:25 +0800

    add ui

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -18,13 +18,38 @@ contract Election {
     // maps can't determine size;
     uint256 public candidatesCount;
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
     constructor() public {
-        addCandidate("Hello");
-        addCandidate("World");
+        addCandidate("Joe Bidden");
+        addCandidate("Hunter Bidden");
     }
 
     function addCandidate(string memory name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
