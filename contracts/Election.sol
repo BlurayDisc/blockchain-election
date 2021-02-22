178d713c602a92f60d46abf596ce83ab5b0da832
Blu <run_y_an@hotmail.com>
2021-02-23 02:46:52 +0800

    update helpers and error msg

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,14 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 // Truffle console helper:
-// 1. Election.deployed().then(function(i) { app=i;})
-// 2. app.candidates(1).then(function(c){candidate = c;})
-// 3. candidate.voteCount.toNumber()
+
+// Election.deployed().then(function(i) { app=i;})
+
+// app.candidates(1).then(function(c){candidate = c;})
+// candidate.voteCount.toNumber()
+
+// let accounts = await web3.eth.getAccounts();
+// app.vote(1, { from: accounts[0] })
 contract Election {
     struct Candidate {
         uint256 id;
@@ -36,18 +41,18 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
     }
 
-    function vote (uint _candidateId) public {
+    function vote(uint _candidateId) public {
         // require that they haven't voted before
-        require(!voters[msg.sender]);
+        require(!voters[msg.sender], 'You have already casted your vote.');
 
         // require a valid candidate
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        require(_candidateId > 0 && _candidateId <= candidatesCount, 'Candidate id is invalid');
 
         // record that voter has voted
         voters[msg.sender] = true;
 
         // update candidate vote Count
-        candidates[_candidateId].voteCount ++;
+        candidates[_candidateId].voteCount++;
 
         // trigger voted event
         emit votedEvent(_candidateId);
